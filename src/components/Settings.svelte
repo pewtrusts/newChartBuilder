22f555c4610ec4f21f6badb1e640dabdbabe0e53
John Osterman <josterman@pewtrusts.org>
2021-02-05 16:19:19 -0500

    WIP text editor

--- src/components/Settings.svelte
+++ src/components/Settings.svelte
@@ -1,6 +1,16 @@
 <script>
+    import sanitizeHtml from 'sanitize-html';
+    import 'quill/dist/quill.core.css';
+    import 'quill/dist/quill.snow.css';
+    import Quill from 'quill';
     import brandOptions from "./../brand-options.json";
     import {ChartCredit, ChartDescription, ChartLabel, ChartNotes, ChartTitle, ChartSources, ChartSubtitle} from './../store';
+    let sanitizeOptions = {
+        allowedTags: [ 'b', 'i', 'em', 'strong', 'a', 'ul', 'ol', 'li'],
+        allowedAttributes: {
+            'a': [ 'href' ]
+        }
+    };
     let mapStores = {
         'chart-credit': ChartCredit,
         'chart-description': ChartDescription,
@@ -23,6 +33,20 @@
             console.log(name,value);
             mapStores[name].set(value);
         }
+    }
+    function initQuill(node){
+        const formats = ['bold','italic','link', 'list'];
+        const editor = new Quill(node, {
+            theme: 'snow',
+            formats,
+            modules: {
+                toolbar: ['bold','italic','link', 'list', { 'list': 'ordered'}, { 'list': 'bullet' }, 'clean']
+            }
+        });
+        editor.on('text-change', function() {
+            console.log(editor.root.innerHTML);
+            ChartNotes.set(sanitizeHtml(editor.root.innerHTML, sanitizeOptions));
+        });
     }
     ChartCredit.subscribe(v => {
         chartCredit = v;
@@ -59,13 +83,19 @@
     input::placeholder {
         color: #767676;
     }
+    .quill-container {
+        width: 100%;
+        height: 300px;
+    }
 </style>
 <form on:submit|preventDefault="{submitHandler}">
     <label>{brandOptions.chartLabelName}:<br /><input bind:value="{chartLabel}" placeholder="e.g., Figure 1" name="chart-label" type="text"></label>
     <label>{brandOptions.chartTitleName}:<br /><input bind:value="{chartTitle}" placeholder="e.g., Most State Tax Revenue Comes from Personal Income Tax and Sales Tax" name="chart-title" type="text"></label>
     <label>{brandOptions.chartSubtitleName}:<br /><input bind:value="{chartSubtitle}" placeholder="e.g., Mix of tax sources by state" name="chart-subtitle" type="text"></label>
     <label>Description:<br /><input bind:value="{chartDescription}" placeholder="A detailed description for screen readers and search engines" name="chart-description" type="text"></label>
-    <label>Notes:<br /><input bind:value="{chartNotes}" placeholder="e.g., Note: Data for 2020 is tentative" name="chart-notes" type="text"></label>
+    <label>Notes:<br /><textarea bind:value="{chartNotes}" placeholder="e.g., Note: Data for 2020 is tentative" name="chart-notes" type="text"></textarea></label>
+    <p class="label">Notes:</p>
+    <div class="quill-container" use:initQuill></div>
     <label>Source:<br /><input bind:value="{chartSources}" placeholder="e.g., Source: John Adams, ABCs Are Easy, 1955" name="chart-sources" type="text"></label>
     <label>Credit:<br /><input bind:value="{chartCredit}" placeholder="e.g., © 2021 The Pew Charitable Trusts" name="chart-credit" type="text"></label>
     <input type="submit">
