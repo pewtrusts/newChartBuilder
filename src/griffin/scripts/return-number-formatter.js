6cc953a47e429661806c7e4bf7efe8e6281b3954
John Osterman <josterman@pewtrusts.org>
2021-03-23 14:25:31 -0400

    names function in return formatter

--- src/griffin/scripts/return-number-formatter.js
+++ src/griffin/scripts/return-number-formatter.js
@@ -2,19 +2,20 @@
 export default function _returnFormatter(format, context){
     switch (format){
         case 'percentage':
-            return function () {
+            return function _percentage() {
                 var value = this.value !== undefined ? this.value : this.y;
                 // TO DO  figure out decimals programmatically
                 var rtn = Highcharts.numberFormat(value * 100, (context == 'tooltip' ? 1 : 0)) + '%';
+                console.log({value,rtn});
                 return rtn;
             };
         case 'currency':
-            return function () {
+            return function _currency() {
                 var value = this.value !== undefined ? this.value : this.y;
                 return '$' + Highcharts.numberFormat(value, 2);
             };
         default:
-            return function () {
+            return function _default() {
                 var value = this.value !== undefined ? this.value : this.y;
                 return Highcharts.numberFormat(value, -1);
             };
