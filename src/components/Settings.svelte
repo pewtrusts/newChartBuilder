e1ecc2e2469643577cb345a2595ecb29dd1814a1
John Osterman <josterman@pewtrusts.org>
2021-10-21 13:34:59 -0400

    Adds spacing bottom and left and rotation

--- src/components/Settings.svelte
+++ src/components/Settings.svelte
@@ -10,19 +10,24 @@
     }
 </script>
 <script>
-
+    let chartType;
     let stacking;
     let reverseStacks;
     let notices = new Set();
+    let rotation;
     let spacingRight;
     let spacingTop;
+    let spacingBottom;
+    let spacingLeft;
     let stackingNotice = {
         label: 'Reversing stacks?',
         description: 'Reversing stacks is not working as expected in Highcharts 8.2.2 when the series are stacked vertically. ' +
         'You may go to the data section and reverse the series there. You may also want to reverse the legend.',
         type: 'warning'
     };
-    
+    s.ChartType.subscribe(v => {
+        chartType = v;
+    });
     s.Stacking.subscribe(v => {
         stacking = v;
         requestIdleCallback(checkMessage, {timeout: 500});
@@ -37,11 +42,29 @@
     s.SpacingTop.subscribe(v => {
         spacingTop = v;
     });
+    s.SpacingBottom.subscribe(v => {
+        spacingBottom = v;
+    });
+    s.SpacingLeft.subscribe(v => {
+        spacingLeft = v;
+    });
+    s.Rotation.subscribe(v => {
+        rotation = v;
+    });
     function spacingRightHandler(){
         s.SpacingRight.set(+this.value);
     }
     function spacingTopHandler(){
         s.SpacingTop.set(+this.value);
+    }
+    function spacingBottomHandler(){
+        s.SpacingBottom.set(+this.value);
+    }
+    function spacingLeftHandler(){
+        s.SpacingLeft.set(+this.value);
+    }
+    function rotationHandler(){
+        s.Rotation.set(+this.value);
     }
     function checkMessage(){
         notices[stacking !== 'none' && reverseStacks ? 'add' : 'delete'](stackingNotice);
@@ -60,6 +83,11 @@
 <Notices {notices} position="left" />
 <label>Spacing top: <input name="top" on:change="{spacingTopHandler}" type="number" bind:value="{spacingTop}"></label>
 <label>Spacing right: <input name="right" on:change="{spacingRightHandler}" type="number" bind:value="{spacingRight}"></label>
+<label>Spacing bottom: <input name="bottom" on:change="{spacingBottomHandler}" type="number" bind:value="{spacingBottom}"></label>
+<label>Spacing left: <input name="bottom" on:change="{spacingLeftHandler}" type="number" bind:value="{spacingLeft}"></label>
+{#if chartType == 'pie'}
+<label>Rotation: <input name="rotation" on:change="{rotationHandler}" type="number" bind:value="{rotation}"></label>
+{/if}
 <label for="stacking-selector">Stacking:</label>
 <!-- svelte-ignore a11y-no-onchange -->
 <select on:change="{stackingHandler}" name="stacking" id="stacking-selector" bind:value="{stacking}">
